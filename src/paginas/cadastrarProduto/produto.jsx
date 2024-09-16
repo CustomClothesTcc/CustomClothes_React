5df344a39047cdb0b7e4e015ce8360d7e4fcc36e
KaiqueVfreitas <kaique.vieira2502eu@outlook.com>
2024-09-16 13:50:50 -0300

    Adicionando um arquivo ao gitignore

--- src/paginas/cadastrarProduto/produto.jsx
+++ src/paginas/cadastrarProduto/produto.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import "./style.scss";
 import BarraLateral from "../../componentes/barraLateral/BarraLateral";
 import BarraDeNavegacao from "../../componentes/barraDeNavegacao/BarraDeNavegacao";
@@ -8,64 +9,82 @@ const CadastrarProduto = ({ entradas, titulo }) => {
   const [arquivo, setArquivo] = useState("");
 
   return (
-    <div className="cadastrarProduto">
+    <div className="pagina">
       <BarraLateral />
-      <div className="containerCadastrarProduto">
+      <div className="conteudo">
         <BarraDeNavegacao />
-        <div className="topo">
-          <h1>{titulo}</h1>
-        </div>
-        <div className="inputFormulario imagemCentralizada">
-          <img
-            src={
-              arquivo
-                ? URL.createObjectURL(arquivo)
-                : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-            }
-            alt="Imagem do Produto"
-            className="imagemProduto"
-          />
-          <label htmlFor="arquivo" className="uploadLabel">
-            Imagem: <DriveFolderUploadOutlinedIcon className="icone" />
-          </label>
-          <input
-            type="file"
-            id="arquivo"
-            onChange={(e) => setArquivo(e.target.files[0])}
-            style={{ display: "none" }}
-          />
-        </div>
-        <div className="baixo">
-          <div className="direita">
-            <form>
-              {entradas.map((entrada) => (
-                <div className="inputFormulario" key={entrada.id}>
-                  <label>{entrada.label}</label>
-                  {entrada.tipo === "select" ? (
-                    <select>
-                      {entrada.options.map((option) => (
-                        <option key={option} value={option}>
-                          {option}
-                        </option>
-                      ))}
-                    </select>
-                  ) : (
-                    <input
-                      type={entrada.tipo}
-                      placeholder={entrada.placeholder}
-                    />
-                  )}
-                </div>
-              ))}
-            </form>
+        <div className="paginaProduto">
+          <div className="containerCadastrarProduto">
+            <div className="topo">
+              <h1>{titulo}</h1>
+            </div>
+            <div className="inputFormulario imagemCentralizada">
+            <img
+                src={
+                  arquivo
+                    ? URL.createObjectURL(arquivo)
+                    : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                }
+                alt="Imagem do Produto"
+                className="imagemProduto"
+              />
+              <label htmlFor="arquivo" className="uploadLabel">
+                Imagem: <DriveFolderUploadOutlinedIcon className="icone" />
+              </label>
+              <input
+                type="file"
+                id="arquivo"
+                onChange={(e) => setArquivo(e.target.files[0])}
+                style={{ display: "none" }}
+              />
+            </div>
+            <div className="baixo">
+              <div className="direita">
+                <form>
+                  {entradas.map((entrada) => (
+                    <div className="inputFormulario" key={entrada.id}>
+                      <label>{entrada.label}</label>
+                      {entrada.tipo === "select" ? (
+                        <select>
+                          {entrada.options.map((option) => (
+                            <option key={option} value={option}>
+                              {option}
+                            </option>
+                          ))}
+                        </select>
+                      ) : (
+                        <input
+                          type={entrada.tipo}
+                          placeholder={entrada.placeholder}
+                        />
+                      )}
+                    </div>
+                  ))}
+                </form>
+              </div>
+            </div>
+            <div className="divButton">
+              <button type="submit">Enviar</button>
+            </div>
           </div>
-        </div>
-        <div className="divButton">
-          <button type="submit">Enviar</button>
         </div>
       </div>
     </div>
   );
+};
+
+// Adicionando validação de props
+CadastrarProduto.propTypes = {
+  entradas: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      label: PropTypes.string.isRequired,
+      tipo: PropTypes.string.isRequired,
+      placeholder: PropTypes.string,
+      options: PropTypes.arrayOf(PropTypes.string)
+    })
+  ).isRequired,
+  titulo: PropTypes.string.isRequired
 };
 
 export default CadastrarProduto;
